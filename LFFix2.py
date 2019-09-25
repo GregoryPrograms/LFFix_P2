676f9cf7021b40d3435bbf8ffa363813f650fa6c
GregoryPrograms <greg.dost@yahoo.com>
2019-09-25 09:09:23 -0700

    Added functionality for fixing folder names

--- LFFix2.py
+++ LFFix2.py
@@ -2,6 +2,11 @@ import os, tkinter
 from tkinter import Tk, filedialog
 from pathlib import Path,PurePath
 BADDICTIONARY = '#,/`~'
+def foldFix(foldList):
+    for folders in reversed(foldList):
+        fixedFolder = folders[0].translate({ord(k):None for k in BADDICTIONARY})
+        os.rename(PurePath(folders[1])/folders[0], PurePath(folders[1])/fixedFolder)
+
 def fileFix(fileList):
     for files in fileList:
         fixedFile = files[0].translate({ord(k):None for k in BADDICTIONARY})
@@ -11,10 +16,15 @@ def main():
     tkinter.Tk().withdraw()
     dirPath = Path(filedialog.askdirectory(title = 'Select directory to trim file names...'))
     fileList = []
-    for root, _, files in os.walk(dirPath):
+    foldList = []
+    for root, folders, files in os.walk(dirPath):
+        for name in folders:
+            foldObj = [name, root]
+            foldList.append(foldObj)
         for name in files:
             fileObj = [name, root]
             fileList.append(fileObj)
     fileFix(fileList)
+    foldFix(foldList)
 if(__name__ == "__main__"):
     main()
